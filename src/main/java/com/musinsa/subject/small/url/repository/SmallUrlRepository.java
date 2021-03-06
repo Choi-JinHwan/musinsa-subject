f1dccba834c274aaed87e1f63dc0db3f435fbd2a
Choi-JinHwan <jinhwan0515@gamil.com>
2021-03-06 23:42:03 +0900

    feat: add SmallUrlRepository#findByHash method

--- src/main/java/com/musinsa/subject/small/url/repository/SmallUrlRepository.java
+++ src/main/java/com/musinsa/subject/small/url/repository/SmallUrlRepository.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Repository;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @Repository
 @Validated
@@ -26,6 +28,10 @@ public class SmallUrlRepository {
     public SmallUrl save(@Valid SmallUrl smallUrl) {
         this.datasource.put(smallUrl.getHash(), smallUrl);
         return smallUrl;
+    }
+
+    public Optional<SmallUrl> findByHash(@Valid @NotBlank String hash) {
+        return Optional.ofNullable(datasource.get(hash));
     }
 
 }
--- src/test/java/com/musinsa/subject/small/url/repository/SmallUrlRepositoryTest.java
+++ src/test/java/com/musinsa/subject/small/url/repository/SmallUrlRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.musinsa.subject.small.url.repository;
 
 import com.musinsa.subject.small.url.domain.SmallUrl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -20,6 +21,16 @@ class SmallUrlRepositoryTest {
 
     @Autowired
     SmallUrlRepository smallUrlRepository;
+
+    private SmallUrl savedSmallUrl;
+
+    @BeforeEach
+    void beforeEach() {
+        var originalUrl = "https://www.wanted.co.kr/newintro";
+        var hash = String.format("%08x", originalUrl.hashCode());
+
+        this.savedSmallUrl = smallUrlRepository.save(new SmallUrl(hash, originalUrl));
+    }
 
     @Test
     @DisplayName("SmallUrl을 저장할 수 있다.")
@@ -64,6 +75,37 @@ class SmallUrlRepositoryTest {
                 ConstraintViolationException.class,
                 () -> smallUrlRepository.save(smallUrl)
         );
+    }
+
+    @Test
+    @DisplayName("hash값으로 SmallUrl을 찾을 수 있다")
+    void findByHash() {
+        // Given
+        var expectedHash = savedSmallUrl.getHash();
+        var expectedOriginalUrl = savedSmallUrl.getOriginalUrl();
+
+        // When
+        var optionalSmallUrl = smallUrlRepository.findByHash(expectedHash);
+
+        // Then
+        assertThat(optionalSmallUrl).isPresent();
+        var smallUrl = optionalSmallUrl.get();
+        assertThat(smallUrl.getHash()).hasSize(8).isNotBlank();
+        assertThat(smallUrl.getOriginalUrl()).isEqualTo(expectedOriginalUrl);
+        assertThat(smallUrl.getRedirectCount()).isGreaterThanOrEqualTo(0L);
+    }
+
+    @Test
+    @DisplayName(" 존재하지 않는 hash값으로 SmallUrl을 찾을 수 없다")
+    void findNotExistSmallUrlByHash() {
+        // Given
+        var expectedHash = "11111111";
+
+        // When
+        var optionalSmallUrl = smallUrlRepository.findByHash(expectedHash);
+
+        // Then
+        assertThat(optionalSmallUrl).isEmpty();
     }
 
 }
