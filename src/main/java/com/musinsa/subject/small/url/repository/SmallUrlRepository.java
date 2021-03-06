e5854b4edd3d262eb867c4b2d750b2aeac2e71c7
Choi-JinHwan <jinhwan0515@gamil.com>
2021-03-06 22:08:13 +0900

    feat: constraint validation SmallUrl before save

--- src/main/java/com/musinsa/subject/small/url/repository/SmallUrlRepository.java
+++ src/main/java/com/musinsa/subject/small/url/repository/SmallUrlRepository.java
@@ -3,11 +3,14 @@ package com.musinsa.subject.small.url.repository;
 
 import com.musinsa.subject.small.url.domain.SmallUrl;
 import org.springframework.stereotype.Repository;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
 @Repository
+@Validated
 public class SmallUrlRepository {
 
     private final Map<String, SmallUrl> datasource;
@@ -20,7 +23,7 @@ public class SmallUrlRepository {
         this.datasource = datasource;
     }
 
-    public SmallUrl save(SmallUrl smallUrl) {
+    public SmallUrl save(@Valid SmallUrl smallUrl) {
         this.datasource.put(smallUrl.getHash(), smallUrl);
         return smallUrl;
     }
