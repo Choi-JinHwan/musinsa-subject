ca4adae1bdc22691c912ae2c639d430716a6eba6
Choi-JinHwan <jinhwan0515@gamil.com>
2021-03-08 14:26:48 +0900

    fix: update controller api description

--- src/main/java/com/musinsa/subject/small/url/interfaces/SmallUrlController.java
+++ src/main/java/com/musinsa/subject/small/url/interfaces/SmallUrlController.java
@@ -4,6 +4,7 @@ import com.musinsa.subject.small.url.domain.SmallUrl;
 import com.musinsa.subject.small.url.service.SmallUrlService;
 import com.musinsa.subject.small.url.vo.SmallUrlCreateRequest;
 import com.musinsa.subject.web.SuccessResponse;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -20,6 +21,7 @@ public class SmallUrlController {
     }
 
     @PostMapping("/small-url")
+    @ApiOperation(value = "SmallUrl을 저장한다")
     public SuccessResponse<SmallUrl> createSmallUrl(
             @Valid @RequestBody SmallUrlCreateRequest request
     ) {
@@ -27,6 +29,7 @@ public class SmallUrlController {
     }
 
     @GetMapping("/{hash:\\w{8}}")
+    @ApiOperation(value = "hash가 같은 SmallUrl의 originalUrl로 리다이렉션한다")
     public void redirect(
             @PathVariable String hash,
             HttpServletResponse response
